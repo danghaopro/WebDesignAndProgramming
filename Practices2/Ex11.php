6061c1138b24a9f357a05ac3407e85e747eec6d1
Nguyễn Đăng Hào <danghaopro97@gmail.com>
2018-03-15 19:48:33 +0700

    Add Name And ID

--- Practices2/Ex11.php
+++ Practices2/Ex11.php
@@ -5,6 +5,7 @@
     <title>Exercises 11</title>
   </head>
   <body>
+    <h1>Nguyễn Đăng Hào - 201554470</h1>
     <?php
       for ($i = 1; $i <= 200; $i++) {
         if ($i % 2 == 0) {// Chia het cho 2
